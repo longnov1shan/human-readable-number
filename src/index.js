7465fe08318ec4ce57c6088cac7ca31bfacc3025
longnov1shan <gurov91@gmail.com>
2023-11-03 22:33:41 +0100

    refactor:newLogic

--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
-module.exports = function toReadable (num) {
-    
-        let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-                    'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-                    'seventeen', 'eighteen', 'nineteen'];
-        let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
-                    'ninety'];
-      
-        let numString = num.toString();
-      
-        if (num < 0) throw new Error('Negative numbers are not supported.');
-      
-        if (num === 0) return 'zero';
-      
-        //the case of 1 - 20
-        if (num < 20) {
-          return ones[num];
-        }
-      
-        if (numString.length === 2) {
-          return tens[numString[0]] + ' ' + ones[numString[1]];
-        }
-      
-        //100 and more
-        if (numString.length == 3) {
-          if (numString[1] === '0' && numString[2] === '0')
-            return ones[numString[0]] + ' hundred';
-          else
-            return ones[numString[0]] + ' hundred and ' + convert(+(numString[1] + numString[2]));
-        }
-      
-        if (numString.length === 4) {
-          let end = +(numString[1] + numString[2] + numString[3]);
-          if (end === 0) return ones[numString[0]] + ' thousand';
-          if (end < 100) return ones[numString[0]] + ' thousand and ' + convert(end);
-          return ones[numString[0]] + ' thousand ' + convert(end);
-        }
-      }
+module.exports = function toReadable(num) {
+  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
- 
+  if (num < 0) throw new Error('Negative numbers are not supported.');
+
+  if (num === 0) return 'zero';
+
+  if (num < 10) return ones[num];
+  if (num < 20) return teens[num - 10];
+
+  const numString = num.toString();
+  const hundredsDigit = parseInt(numString[0]);
+  const tensDigit = parseInt(numString[1]);
+  const onesDigit = parseInt(numString[2]);
+
+  let result = '';
+
+  if (hundredsDigit > 0) {
+      result += ones[hundredsDigit] + ' hundred';
+      if (tensDigit > 0 || onesDigit > 0) {
+          result += ' ';
+      }
+  }
+
+  if (tensDigit > 0) {
+      if (tensDigit === 1) {
+          result += teens[onesDigit];
+      } else {
+          result += tens[tensDigit];
+          if (onesDigit > 0) {
+              result += ' ';
+          }
+      }
+  }
+
+  if (onesDigit > 0 && tensDigit !== 1) {
+      result += ones[onesDigit];
+  }
+
+  return result;
+};
