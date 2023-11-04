8e5eec57b6604d67f17dc69f4330aeb4c5949ba6
longnov1shan <gurov91@gmail.com>
2023-11-05 00:48:22 +0100

    refactor:MathLogic

--- src/index.js
+++ src/index.js
@@ -1,43 +1,36 @@
-module.exports = function toReadable(num) {
-  const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-
-  if (num < 0) throw new Error('Negative numbers are not supported.');
-
-  if (num === 0) return 'zero';
-
-  if (num < 10) return ones[num];
-  if (num < 20) return teens[num - 10];
-
-  const numString = num.toString();
-  const hundredsDigit = parseInt(numString[0]);
-  const tensDigit = parseInt(numString[1]);
-  const onesDigit = parseInt(numString[2]);
-
-  let result = '';
-
-  if (hundredsDigit > 0) {
-      result += ones[hundredsDigit] + ' hundred';
-      if (tensDigit > 0 || onesDigit > 0) {
-          result += ' ';
-      }
-  }
-
-  if (tensDigit > 0) {
-      if (tensDigit === 1) {
-          result += teens[onesDigit];
-      } else {
-          result += tens[tensDigit];
-          if (onesDigit > 0) {
-              result += ' ';
-          }
-      }
-  }
-
-  if (onesDigit > 0 && tensDigit !== 1) {
-      result += ones[onesDigit];
-  }
-
-  return result;
-};
+module.exports = function toReadable(n) {
+    if (n < 0)
+      return false;
+	 single_digit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+	 double_digit = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+	 below_hundred = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
+	if (n === 0) return 'zero'
+	function translate(n) {
+		word = ""
+		if (n < 10) {
+			word = single_digit[n] + ' '
+		}
+		else if (n < 20) {
+			word = double_digit[n - 10] + ' '
+		}
+		else if (n < 100) {
+			rem = translate(n % 10)
+			word = below_hundred[(n - n % 10) / 10 - 2] + ' ' + rem
+		}
+		else if (n < 1000) {
+			word = single_digit[Math.trunc(n / 100)] + ' hundred ' + translate(n % 100)
+		}
+		else if (n < 1000000) {
+			word = translate(parseInt(n / 1000)).trim() + ' Thousand ' + translate(n % 1000)
+		}
+		else if (n < 1000000000) {
+			word = translate(parseInt(n / 1000000)).trim() + ' Million ' + translate(n % 1000000)
+		}
+		else {
+			word = translate(parseInt(n / 1000000000)).trim() + ' Billion ' + translate(n % 1000000000)
+		}
+		return word
+	}
+	 result = translate(n) 
+	return result.trim()+'.'
+}
