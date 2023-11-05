b0eb9ae45cac576eeb705bf6e78b9365ca0662f8
longnov1shan <gurov91@gmail.com>
2023-11-05 22:44:11 +0100

    refactor:newCondit

--- src/index.js
+++ src/index.js
@@ -1,36 +1,39 @@
-module.exports = function toReadable(n) {
-    if (n < 0)
-      return false;
-	 single_digit = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-	 double_digit = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
-	 below_hundred = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety']
-	if (n === 0) return 'zero'
-	function translate(n) {
-		word = ""
-		if (n < 10) {
-			word = single_digit[n] + ' '
-		}
-		else if (n < 20) {
-			word = double_digit[n - 10] + ' '
-		}
-		else if (n < 100) {
-			rem = translate(n % 10)
-			word = below_hundred[(n - n % 10) / 10 - 2] + ' ' + rem
-		}
-		else if (n < 1000) {
-			word = single_digit[Math.trunc(n / 100)] + ' hundred ' + translate(n % 100)
-		}
-		else if (n < 1000000) {
-			word = translate(parseInt(n / 1000)).trim() + ' Thousand ' + translate(n % 1000)
-		}
-		else if (n < 1000000000) {
-			word = translate(parseInt(n / 1000000)).trim() + ' Million ' + translate(n % 1000000)
-		}
-		else {
-			word = translate(parseInt(n / 1000000000)).trim() + ' Billion ' + translate(n % 1000000000)
-		}
-		return word
-	}
-	 result = translate(n) 
-	return result.trim()+'.'
-}
+module.exports = function toReadable(number) {
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+    if (number < 0) throw new Error('Negative numbers are not supported.');
+
+    if (number === 0) return 'zero';
+
+    if (number < 10) return ones[number];
+    if (number < 20) return teens[number - 10];
+
+    const numString = number.toString();
+    const hundredsDigit = parseInt(numString[0]);
+    const tensDigit = parseInt(numString[1]);
+    const onesDigit = parseInt(numString[2]);
+
+    let result = '';
+
+    if (hundredsDigit !== 0) {
+         result += ones[hundredsDigit] + ' hundred';
+      if (tensDigit > 0 || onesDigit > 0) {
+        result += ' ';
+      }
+    }
+
+    if (tensDigit >= 2 && tensDigit <= 9 ) {
+      result += tens[tensDigit];
+      if (onesDigit > 0) {
+        result += ' ' + ones[onesDigit];
+      }
+    } else if (tensDigit === 1) {
+      result += teens[onesDigit];
+    } else if (onesDigit > 0) {
+      result += ones[onesDigit];
+    }
+
+    return result;
+};
