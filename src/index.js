3d44c91c9795649749dedeb0ca2b812254c298cd
longnov1shan <gurov91@gmail.com>
2023-11-06 10:53:47 +0100

    refactor:final

--- src/index.js
+++ src/index.js
@@ -1,39 +1,110 @@
 module.exports = function toReadable(number) {
-    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-    const tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
-    if (number < 0) throw new Error('Negative numbers are not supported.');
+    console.log("number:", number);
 
-    if (number === 0) return 'zero';
-
-    if (number < 10) return ones[number];
-    if (number < 20) return teens[number - 10];
-
-    const numString = number.toString();
-    const hundredsDigit = parseInt(numString[0]);
-    const tensDigit = parseInt(numString[1]);
-    const onesDigit = parseInt(numString[2]);
-
-    let result = '';
-
-    if (hundredsDigit !== 0) {
-         result += ones[hundredsDigit] + ' hundred';
-      if (tensDigit > 0 || onesDigit > 0) {
-        result += ' ';
-      }
+    if (number === 0) {
+        console.log("Returning 'zero'");
+        return 'zero';
     }
 
-    if (tensDigit >= 2 && tensDigit <= 9 ) {
-      result += tens[tensDigit];
-      if (onesDigit > 0) {
-        result += ' ' + ones[onesDigit];
-      }
-    } else if (tensDigit === 1) {
-      result += teens[onesDigit];
-    } else if (onesDigit > 0) {
-      result += ones[onesDigit];
+    let stringNumber = String(number);
+    let lengthValue = stringNumber.length;
+    let result;
+
+    const simple = [
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+    ];
+    const unique = [
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+    ];
+    const dozens = [
+        'ten',
+        'twenty',
+        'thirty',
+        'forty',
+        'fifty',
+        'sixty',
+        'seventy',
+        'eighty',
+        'ninety',
+    ];
+    const hundreds = [
+        'one hundred',
+        'two hundred',
+        'three hundred',
+        'four hundred',
+        'five hundred',
+        'six hundred',
+        'seven hundred',
+        'eight hundred',
+        'nine hundred',
+    ];
+
+    console.log("lengthValue:", lengthValue);
+
+    if (lengthValue === 1) {
+        result = simple[number - 1];
+        console.log("Returning result:", result);
+    }
+
+    if (lengthValue === 2) {
+
+        if (number >= 11 && number <= 19) {
+            result = unique[number - 11];
+            console.log("Returning result:", result);
+        } else if (number % 10 === 0) {
+            result = dozens[stringNumber[0] - 1];
+            console.log("Returning result:", result);
+        } else {
+            result = `${dozens[stringNumber[0] - 1]} ${simple[stringNumber[1] - 1]}`;
+            console.log("Returning result:", result);
+        }
+    }
+
+    if (lengthValue === 3) {
+
+        if (number % 100 === 0) {
+            result = hundreds[stringNumber[0] - 1];
+            console.log("Returning result:", result);
+        } else if (stringNumber[1] == 0) {
+            result = `${hundreds[stringNumber[0] - 1]} ${simple[stringNumber[2] - 1]}`;
+            console.log("Returning result:", result);
+        } else {
+
+            let twoDigitNumberStr = `${stringNumber[1]}${stringNumber[2]}`;
+
+            let twoDigitNumber = Number(twoDigitNumberStr);
+
+            if (twoDigitNumber % 10 == 0) {
+                result = `${hundreds[stringNumber[0] - 1]} ${dozens[twoDigitNumberStr[0] - 1]}`;
+                console.log("Returning result:", result);
+            } else if (twoDigitNumber >= 11 && twoDigitNumber <= 19) {
+                result = `${hundreds[stringNumber[0] - 1]} ${unique[twoDigitNumber - 11]}`;
+                console.log("Returning result:", result);
+            } else {
+                result = `${hundreds[stringNumber[0] - 1]} ${dozens[twoDigitNumberStr[0] - 1]} ${simple[twoDigitNumberStr[1] - 1]}`;
+                console.log("Returning result:", result);
+            }
+        }
     }
 
     return result;
-};
+}
+let number = 29; 
+console.log(number);
